c1b6299852cf2df855c3ada125354bda156a756c
Auston Stewart <auston@shedbuilt.net>
2018-01-11 13:59:37 -0500

    Updating to 4.14.13

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 make mrproper
-mkdir -v ${SHED_FAKEROOT}/usr
-make INSTALL_HDR_PATH=${SHED_FAKEROOT}/usr headers_install
-find ${SHED_FAKEROOT}/usr/include \( -name .install -o -name ..install.cmd \) -delete
+case "$SHED_BUILDMODE" in
+    toolchain)
+        mkdir -v "${SHED_FAKEROOT}/tools"
+        make INSTALL_HDR_PATH="${SHED_FAKEROOT}/tools" headers_install
+    ;;
+    *)
+        mkdir -v "${SHED_FAKEROOT}/usr"
+        make INSTALL_HDR_PATH="${SHED_FAKEROOT}/usr" headers_install
+        find ${SHED_FAKEROOT}/usr/include \( -name .install -o -name ..install.cmd \) -delete
+    ;;
+esac
