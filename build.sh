f234a4c057b92fe2a8b6ee498edc940125843192
Auston Stewart <auston@shedbuilt.net>
2018-03-14 10:38:02 -1000

    Updating to 4.16rc5

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-make mrproper
 case "$SHED_BUILDMODE" in
     toolchain)
-        mkdir -v "${SHED_FAKEROOT}/tools"
+        mkdir -v "${SHED_FAKEROOT}/tools" &&
         make INSTALL_HDR_PATH="${SHED_FAKEROOT}/tools" headers_install
     ;;
     *)
-        mkdir -v "${SHED_FAKEROOT}/usr"
-        make INSTALL_HDR_PATH="${SHED_FAKEROOT}/usr" headers_install
-        find ${SHED_FAKEROOT}/usr/include \( -name .install -o -name ..install.cmd \) -delete
+        mkdir -v "${SHED_FAKEROOT}/usr" &&
+        make INSTALL_HDR_PATH="${SHED_FAKEROOT}/usr" headers_install &&
+        find "${SHED_FAKEROOT}/usr/include" \( -name .install -o -name ..install.cmd \) -delete
     ;;
 esac
