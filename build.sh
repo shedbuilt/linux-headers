479ec0b1feb23269d6859234a68d88837d36a0b1
Auston Stewart <auston@shedbuilt.net>
2018-05-22 16:58:50 -1000

    Updating for blank package options

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-case "$SHED_BUILD_MODE" in
-    toolchain)
-        mkdir -v "${SHED_FAKE_ROOT}/tools" &&
-        make INSTALL_HDR_PATH="${SHED_FAKE_ROOT}/tools" headers_install
-    ;;
-    *)
-        mkdir -v "${SHED_FAKE_ROOT}/usr" &&
-        make INSTALL_HDR_PATH="${SHED_FAKE_ROOT}/usr" headers_install &&
-        find "${SHED_FAKE_ROOT}/usr/include" \( -name .install -o -name ..install.cmd \) -delete
-    ;;
-esac
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_HEADER_PATH="${SHED_FAKE_ROOT}/usr"
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    SHED_PKG_LOCAL_HEADER_PATH="${SHED_FAKE_ROOT}/tools"
+fi
+
+# Build and Install
+mkdir -v "$SHED_PKG_LOCAL_HEADER_PATH" &&
+make INSTALL_HDR_PATH="$SHED_PKG_LOCAL_HEADER_PATH" headers_install &&
+find "$SHED_PKG_LOCAL_HEADER_PATH"/include" \( -name .install -o -name ..install.cmd \) -delete
